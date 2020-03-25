7763de3176de346fd99eb14ebe1857834660a7de
MatheusLuz <matheusfernandesluz@gmail.com>
2020-03-25 16:51:43 -0400

    Backend da aplicação

---
+++ backend/src/database/migrations/20200325145803_create_ongs.js
@@ -0,0 +1,17 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+
+exports.up = function (knex) {
+  return knex.schema.createTable('ongs', function (table) {
+    table.string('id').primary();
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.string('whatsapp').notNullable();
+    table.string('city').notNullable();
+    table.string('uf', 2).notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  knex.schema.dropTable('ongs');
+};
